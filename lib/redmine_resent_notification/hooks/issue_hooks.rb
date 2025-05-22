1234eb6feebadab812c6992487d6ef9af5a7ac34
DolezalDavid <52913270+DolezalDavid@users.noreply.github.com>
2025-05-22 23:04:45 +0200

    Update issue_hooks.rb

--- lib/redmine_resent_notification/hooks/issue_hooks.rb
+++ lib/redmine_resent_notification/hooks/issue_hooks.rb
@@ -3,48 +3,37 @@ module RedmineResentNotification
     class IssueHooks < Redmine::Hook::ViewListener
       
       def view_issues_show_description_bottom(context = {})
-        issue = context[:issue]
-        return '' unless issue
-        return '' unless User.current.allowed_to?(:resend_notifications, issue.project)
-
-        # Použití standardní Redmine 'mail' ikony
-        link_content = sprite_icon('mail', l(:button_resend_notification))
+        Rails.logger.info "=== HOOK: view_issues_show_description_bottom called ==="
         
-        content_tag(:div, class: 'contextual resent-notification-actions') do
-          link_to(
-            link_content,
-            resend_issue_notification_path(project_id: issue.project, issue_id: issue),
-            method: :post,
-            class: 'icon icon-email',
-            title: l(:button_resend_notification),
-            data: { 
-              confirm: l(:text_resend_notification_confirmation),
-              remote: true
-            }
-          )
-        end
+        issue = context[:issue]
+        Rails.logger.info "=== HOOK: Issue found: #{issue&.id} ==="
+        
+        return '' unless issue
+        
+        user_allowed = User.current.allowed_to?(:resend_notifications, issue.project)
+        Rails.logger.info "=== HOOK: User #{User.current.login} allowed: #{user_allowed} ==="
+        
+        return '' unless user_allowed
+
+        # Zkusit jednoduché HTML místo sprite_icon
+        html_content = %{
+          <div class="contextual" style="float: right; margin-top: -2em;">
+            <a href="/issues/#{issue.id}/resend_notification" 
+               data-method="post" 
+               class="icon icon-email" 
+               style="background: #f0f0f0; padding: 5px 10px; margin-left: 5px; text-decoration: none; border-radius: 3px;"
+               onclick="return confirm('Opravdu chcete znovu odeslat notifikaci?')">
+              📧 Odeslat znovu
+            </a>
+          </div>
+        }.html_safe
+        
+        Rails.logger.info "=== HOOK: Returning HTML ==="
+        html_content
       end
       
-      def view_issues_context_menu_end(context = {})
-        issues = context[:issues]
-        return '' unless issues&.any?
-        
-        # Check permissions for all issues
-        return '' unless issues.all? { |issue| User.current.allowed_to?(:resend_notifications, issue.project) }
-        
-        content_tag(:li) do
-          link_to(
-            sprite_icon('mail', l(:button_resend_notifications_bulk)),
-            '#',
-            class: 'icon icon-email',
-            onclick: 'bulkResentNotifications(); return false;',
-            title: l(:button_resend_notifications_bulk)
-          )
-        end
-      end
-
       def view_layouts_base_html_head(context = {})
-        # Include only plugin stylesheets (SVG ikony nejsou potřeba)
+        Rails.logger.info "=== HOOK: view_layouts_base_html_head called ==="
         stylesheet_link_tag('redmine-resent-notification', plugin: 'redmine_resent_notification')
       end
     end
