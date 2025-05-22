01563761ebe1d509ab1520f95081db25c3fdf24b
DolezalDavid <52913270+DolezalDavid@users.noreply.github.com>
2025-05-22 19:06:20 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
-# Plugin's routes
-post 'resent_notifications/:issue_id/resend_issue', 
+# Redmine Resent Notification Plugin Routes
+
+post 'projects/:project_id/issues/:issue_id/resend_notification', 
      to: 'resent_notifications#resend_issue', 
      as: 'resend_issue_notification'
 
-post 'resent_notifications/:journal_id/resend_journal', 
+post 'projects/:project_id/journals/:journal_id/resend_notification', 
      to: 'resent_notifications#resend_journal', 
      as: 'resend_journal_notification'
 
+# Direct routes (bez project_id)
+post 'issues/:issue_id/resend_notification', 
+     to: 'resent_notifications#resend_issue'
+
+post 'journals/:journal_id/resend_notification', 
+     to: 'resent_notifications#resend_journal'
+
+# Admin routes
 get 'admin/resent_notifications', 
     to: 'resent_notifications#index',
     as: 'admin_resent_notifications'
